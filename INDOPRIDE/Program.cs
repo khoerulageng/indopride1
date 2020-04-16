1ae9a349c2085f474af923f036519626f9fb9927
khoerulageng <46969650+khoerulageng@users.noreply.github.com>
2020-04-16 20:21:49 +0700

    5

--- INDOPRIDE/Program.cs
+++ INDOPRIDE/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 
 namespace INDOPRIDE
 {
@@ -11,12 +15,97 @@ namespace INDOPRIDE
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        private static String SingleAppComEventName = "Make up some unique name here, suggestion: use a GUID";
+        private static BackgroundWorker singleAppComThread = null;
+        private static EventWaitHandle threadComEvent = null;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
         [STAThread]
         static void Main()
         {
+            try
+            {
+                // another instance is already running if OpenExsting succeeds.
+                threadComEvent = EventWaitHandle.OpenExisting(SingleAppComEventName);
+                threadComEvent.Set();  // signal the other instance.
+                threadComEvent.Close();
+                return;    // return immediatly.
+            }
+            catch { /* don't care about errors */     }
+            // Create the Event handle
+            threadComEvent = new EventWaitHandle(false, EventResetMode.AutoReset, SingleAppComEventName);
+            CreateInterAppComThread();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new indopride());
+
+            // End the communication thread.
+            singleAppComThread.CancelAsync();
+            while (singleAppComThread.IsBusy)
+                Thread.Sleep(50);
+            threadComEvent.Close();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        static private void CreateInterAppComThread()
+        {
+            singleAppComThread = new BackgroundWorker();
+            singleAppComThread.WorkerReportsProgress = false;
+            singleAppComThread.WorkerSupportsCancellation = true;
+            singleAppComThread.DoWork += new DoWorkEventHandler(singleAppComThread_DoWork);
+            singleAppComThread.RunWorkerAsync();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static private void singleAppComThread_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            WaitHandle[] waitHandles = new WaitHandle[] { threadComEvent };
+
+            while (!worker.CancellationPending)
+            {
+                // check every second for a signal.
+                if (WaitHandle.WaitAny(waitHandles, 1000) == 0)
+                {
+                    // The user tried to start another instance. We can't allow that, 
+                    // so bring the other instance back into view and enable that one. 
+                    // That form is created in another thread, so we need some thread sync magic.
+                    if (Application.OpenForms.Count > 0)
+                    {
+                        Form mainForm = Application.OpenForms[0];
+                        mainForm.Invoke(new SetFormVisableDelegate(ThreadFormVisable), mainForm);
+                    }
+                }
+            }
+        }
+        // Activate an application window.
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        /// <summary>
+        /// When this method is called using a Invoke then this runs in the thread
+        /// that created the form, which is nice. 
+        /// </summary>
+        /// <param name="frm"></param>
+        private delegate void SetFormVisableDelegate(Form frm);
+        static private void ThreadFormVisable(Form frm)
+        {
+            if (frm != null)
+            {
+                // display the form and bring to foreground.
+                frm.Visible = true;
+                frm.WindowState = FormWindowState.Normal;
+                frm.Show();
+                SetForegroundWindow(frm.Handle);
+                
+            }
         }
     }
 }
